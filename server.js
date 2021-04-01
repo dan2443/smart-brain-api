31e92581f27a36dba785964b24dc65c05c5e5e35
dan2443 <68905500+dan2443@users.noreply.github.com>
2021-04-01 09:50:39 +0300

    Update database

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const image = require("./controllers/image");
 const db = require("knex")({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "dan2443",
+    host: "postgresql-parallel-68889",
+    user: "dan",
+    password: "",
     database: "smart-brain",
   },
 });
