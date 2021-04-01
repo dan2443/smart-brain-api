d7abc128fb0e575a9959f26ce30f83ebe6291493
dan2443 <68905500+dan2443@users.noreply.github.com>
2021-04-01 10:10:26 +0300

    Add database connection

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const image = require("./controllers/image");
 const db = require("knex")({
   client: "pg",
   connection: {
-    host: "postgresql-parallel-68889",
-    user: "dan",
-    password: "",
-    database: "smart-brain",
+    host: "process.env.DATABASE_URL",
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
 
