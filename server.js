1030cf51333dd16ae0d4b7f6d7312b8444b94ebd
dan2443 <68905500+dan2443@users.noreply.github.com>
2021-04-01 10:44:46 +0300

    Add database connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const cors = require("cors");
+//const cors = require("cors");
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
@@ -18,12 +18,14 @@ const db = require("knex")({
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["*"],
-    credentials: true,
-  })
-);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json("it is working!");
