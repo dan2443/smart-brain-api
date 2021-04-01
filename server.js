875c5d4c632ef7dfb32b68fde81d24a27b81075a
dan2443 <68905500+dan2443@users.noreply.github.com>
2021-04-01 10:47:30 +0300

    Update database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-//const cors = require("cors");
+const cors = require("cors");
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
@@ -18,14 +18,7 @@ const db = require("knex")({
 
 const app = express();
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors);
 
 app.get("/", (req, res) => {
   res.json("it is working!");
